765f4c6a44f73163a08b3b588c2f7862f87f8393
BandG <suppersiroo@gmail.com>
2021-03-29 10:34:21 +0700

    templates_1

--- myWeb/myApp1/views.py
+++ myWeb/myApp1/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse                #more
 
 # Create your views here.
 
+class information():
+    def __init__(self,name,age,country,univer):
+        self.name=name
+        self.age=age
+        self.country=country
+        self.univer=univer
+
 def index(request):
-    return HttpResponse("hello worl!!")
+    context={"infor":information("Ban",21,"Bac Ninh, VN","Haui")}
+    return render(request=request,template_name="myApp1/index.html",context=context)     #more
